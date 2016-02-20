d41c634b7079f1b804c1640f03b2cc338a3b9113
Brian Berzellini <berzelba@gmail.com>
2016-02-20 00:42:17 -0500

    changes back to .env file with xcode install

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,10 +6,9 @@ class SearchController < ApplicationController
 			require 'json'
 			require 'rest_client'
 
-			@query = params[:q]
-			# .gsub(/ /, "%20").to_s
+			@query = params[:q].gsub(/ /, "%20").to_s
 
-			RestClient.get("http://food2fork.com/api/search?key=76a1bb1a21eb14d6f5569039fa0e1fe8&q=#{@query}")
+			RestClient.get("http://food2fork.com/api/search?key="+ENV["FOOD2FORK_API_KEY"]+"&q=#{@query}")
 
 			@recipes = JSON.parse(data)['recipes']
   	end
