e2c15be612f65c1f2f8f4e8820d788929a11bc64
Brian Berzellini <berzelba@gmail.com>
2016-02-24 18:42:03 -0500

    changes to env file and added keys to heroku

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,11 +10,8 @@ class SearchController < ApplicationController
 			@search= params[:q]
 
 			@query = URI.encode("#{@search}").to_s
-			# .gsub(/ /, "%20").to_s
 
-# 			@data = RestClient.get("http://food2fork.com/api/search?key="+ENV["FOOD2FORK_API_KEY"]+"&q=#{@query}")
-# 76a1bb1a21eb14d6f5569039fa0e1fe8
-			@data = RestClient.get("http://food2fork.com/api/search?key=76a1bb1a21eb14d6f5569039fa0e1fe8&q=#{@query}")
+			@data = RestClient.get("http://food2fork.com/api/search?key="+ENV["FOOD2FORK_API_KEY"]+"&q=#{@query}")
 
 			@recipes = JSON.parse(@data)['recipes']
 
