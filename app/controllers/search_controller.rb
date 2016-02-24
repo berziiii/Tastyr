72a3a515748b1ce4c328cf3d360c38f9dbc2a114
Brian Berzellini <berzelba@gmail.com>
2016-02-24 16:31:11 -0500

    change to search_controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -15,7 +15,7 @@ class SearchController < ApplicationController
 # 			@data = RestClient.get("http://food2fork.com/api/search?key="+ENV["FOOD2FORK_API_KEY"]+"&q=#{@query}")
 # 76a1bb1a21eb14d6f5569039fa0e1fe8
 			@data = RestClient.get("http://food2fork.com/api/search?key=76a1bb1a21eb14d6f5569039fa0e1fe8&q=#{@query}")
-76a1bb1a21eb14d6f5569039fa0e1fe8
+
 			@recipes = JSON.parse(@data)['recipes']
 
   	end
