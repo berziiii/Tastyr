90044029cc0491808f4b5e2668942c4b1a6a69d1
Brian Berzellini <berzelba@gmail.com>
2016-02-24 15:54:52 -0500

    back to + in api search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -12,7 +12,7 @@ class SearchController < ApplicationController
 			@query = URI.encode("#{@search}").to_s
 			# .gsub(/ /, "%20").to_s
 
-			@data = RestClient.get("http://food2fork.com/api/search?key=ENV["FOOD2FORK_API_KEY"]&q=#{@query}")
+			@data = RestClient.get("http://food2fork.com/api/search?key="+ENV["FOOD2FORK_API_KEY"]+"&q=#{@query}")
 
 			@recipes = JSON.parse(@data)['recipes']
 
