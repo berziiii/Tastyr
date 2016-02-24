928aeef7b16789246b527089e7606826a15ccf30
Brian Berzellini <berzelba@gmail.com>
2016-02-24 16:22:57 -0500

    upgrade to ruby 2.2.3

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -12,8 +12,10 @@ class SearchController < ApplicationController
 			@query = URI.encode("#{@search}").to_s
 			# .gsub(/ /, "%20").to_s
 
-			@data = RestClient.get("http://food2fork.com/api/search?key="+ENV["FOOD2FORK_API_KEY"]+"&q=#{@query}")
-
+# 			@data = RestClient.get("http://food2fork.com/api/search?key="+ENV["FOOD2FORK_API_KEY"]+"&q=#{@query}")
+# 76a1bb1a21eb14d6f5569039fa0e1fe8
+			@data = RestClient.get("http://food2fork.com/api/search?key=76a1bb1a21eb14d6f5569039fa0e1fe8&q=#{@query}")
+76a1bb1a21eb14d6f5569039fa0e1fe8
 			@recipes = JSON.parse(@data)['recipes']
 
   	end
